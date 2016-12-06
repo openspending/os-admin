e982fa782c250413b80e8e5b3fd775411e37ad1f
Levko Kravets <levko.ne@gmail.com>
2016-12-06 15:49:04 +0200

    openspending/openspending#1177 Add filter by loading status

--- app/scripts/services/index.js
+++ app/scripts/services/index.js
@@ -2,4 +2,5 @@
 
 module.exports.ngUtils = require('./ng-utils');
 module.exports.config = require('./config');
+module.exports.filtering = require('./filtering');
 module.exports.osAdmin = require('./os-admin');
