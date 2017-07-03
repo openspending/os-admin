b1ff098021e5eb45efbcc07b672de60c14d5f46f
Vitor Baptista <vitor@vitorbaptista.com>
2017-07-03 17:14:55 +0100

    Use HTTPS versions instead of protocol-relative URLs
    The problem with protocol-relative URLs is that if we're developing locally,
    they'll become `file://` (as there's no server in `os-admin`). Even when
    there's a server, the protocol used is HTTP (because usually there's no HTTPS
    in localhost), which on `next.openspending.org` gets redirected to HTTPS and
    the redirection breaks CORS.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.globalConfig = {
-  conductorUrl: "//next.openspending.org",
-  searchUrl: "//next.openspending.org/search/package",
-  viewerUrl: "//next.openspending.org/viewer",
-  explorerUrl: "//next.openspending.org/explorer",
-  packagerUrl: "//next.openspending.org/packager",
+  conductorUrl: "https://next.openspending.org",
+  searchUrl: "https://next.openspending.org/search/package",
+  viewerUrl: "https://next.openspending.org/viewer",
+  explorerUrl: "https://next.openspending.org/explorer",
+  packagerUrl: "https://next.openspending.org/packager",
   snippets: {
     ga: null
   }
